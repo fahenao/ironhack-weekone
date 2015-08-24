c1525251317b328fd3018a867048fd2cc3405dd6
Felipe Henao <fahenao@MacBook-Pro-de-Felipe.local>
2015-08-24 08:47:43 +0200

    some changes

--- dayFour/ShoppingCart.rb
+++ dayFour/ShoppingCart.rb
@@ -1,16 +1,12 @@
-module CashRegisterFunctions
 
-		def total(shopping_cart)
-			total = shopping_cart.reduce {|sum, x| sum + x.price}
-			puts "Cost: #{total}$"
-		end
+
 
 		# def show_item_list(shopping_cart)
 		# 	shopping_cart.each do |item|
 		# 		puts "shopping_cart.item" 
 		# end
 
-end
+
 
 class ShoppingCart
 
@@ -21,6 +17,12 @@ class ShoppingCart
 
 	def add_to_cart(item)
 		@shopping_cart.push :item
+	end
+
+
+	def total(shopping_cart)
+		total = shopping_cart.reduce {|sum, x| sum + x.price}
+		puts "Cost: #{total}$"
 	end
 end
 
